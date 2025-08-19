3125b51bded5f0aba22e7e1d734f4251b7679f47
parkhar409 <wallibo1818@gmail.com>
2025-08-19 16:31:51 +0900

    Functions added

--- src/ParkingLot.java
+++ src/ParkingLot.java
@@ -1,3 +1,17 @@
+// 
 public class ParkingLot {
-    
+    private ParkingSpot[][] spots;
+
+    public ParkingLot(int rows, int cols) {
+        spots = new ParkingSpot[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                spots[i][j] = new ParkingSpot();
+            }
+        }
+    }
+
+    public ParkingSpot getSpot(int row, int col) {
+        return spots[row][col];
+    }
 }
--- src/ParkingSpot.java
+++ src/ParkingSpot.java
@@ -1,3 +1,3 @@
 public class ParkingSpot {
-    
+    private boolean isOccupied;
 }
